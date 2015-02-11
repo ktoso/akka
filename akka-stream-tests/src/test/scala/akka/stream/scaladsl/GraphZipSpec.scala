de471eed6cae08a19b399819a052a4c4f4c41a5e
Konrad Malawski <konrad.malawski@typesafe.com>
2015-02-11 17:47:10 +0100

    Restored Zip / ZipWith

---
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/GraphZipSpec.scala
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2014 Typesafe Inc. <http://www.typesafe.com>
+ */
+package akka.stream.scaladsl
+
+import akka.stream.scaladsl.FlowGraph.FlowGraphBuilder
+import akka.stream.scaladsl.Graphs.{ OutPort, InPort }
+import akka.stream.scaladsl.FlowGraph.Implicits._
+import akka.stream.testkit.StreamTestKit
+import akka.stream.testkit.TwoStreamsSetup
+
+class GraphZipSpec extends TwoStreamsSetup {
+
+  override type Outputs = (Int, Int)
+
+  override def fixture(b: FlowGraphBuilder): Fixture = new Fixture(b: FlowGraphBuilder) {
+    val zip = Zip[Int, Int](b)
+
+    override def left: InPort[Int] = zip.left
+    override def right: InPort[Int] = zip.right
+    override def out: OutPort[(Int, Int)] = zip.out
+  }
+
+  "Zip" must {
+
+    "work in the happy case" in {
+      val probe = StreamTestKit.SubscriberProbe[(Int, String)]()
+
+      FlowGraph { implicit b ⇒
+        val zip = Zip[Int, String]
+
+        Source(1 to 4) ~> zip.left
+        Source(List("A", "B", "C", "D", "E", "F")) ~> zip.right
+
+        zip.out ~> Sink(probe)
+      }.run()
+
+      val subscription = probe.expectSubscription()
+
+      subscription.request(2)
+      probe.expectNext((1, "A"))
+      probe.expectNext((2, "B"))
+
+      subscription.request(1)
+      probe.expectNext((3, "C"))
+      subscription.request(1)
+      probe.expectNext((4, "D"))
+
+      probe.expectComplete()
+    }
+
+    commonTests()
+
+    "work with one immediately completed and one nonempty publisher" in {
+      val subscriber1 = setup(completedPublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectCompletedOrSubscriptionFollowedByComplete()
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), completedPublisher)
+      subscriber2.expectCompletedOrSubscriptionFollowedByComplete()
+    }
+
+    "work with one delayed completed and one nonempty publisher" in {
+      val subscriber1 = setup(soonToCompletePublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectCompletedOrSubscriptionFollowedByComplete()
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), soonToCompletePublisher)
+      subscriber2.expectCompletedOrSubscriptionFollowedByComplete()
+    }
+
+    "work with one immediately failed and one nonempty publisher" in {
+      val subscriber1 = setup(failedPublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectErrorOrSubscriptionFollowedByError(TestException)
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), failedPublisher)
+      subscriber2.expectErrorOrSubscriptionFollowedByError(TestException)
+    }
+
+    "work with one delayed failed and one nonempty publisher" in {
+      val subscriber1 = setup(soonToFailPublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectErrorOrSubscriptionFollowedByError(TestException)
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), soonToFailPublisher)
+      val subscription2 = subscriber2.expectErrorOrSubscriptionFollowedByError(TestException)
+    }
+
+  }
+
+}
---
+++ akka-stream-tests/src/test/scala/akka/stream/scaladsl/GraphZipWithSpec.scala
@@ -0,0 +1,177 @@
+package akka.stream.scaladsl
+
+import akka.stream.scaladsl.FlowGraph.FlowGraphBuilder
+import akka.stream.scaladsl.FlowGraph.Implicits._
+import akka.stream.scaladsl.Graphs.{ OutPort, InPort }
+import akka.stream.testkit.StreamTestKit
+import akka.stream.testkit.TwoStreamsSetup
+import scala.concurrent.duration._
+
+class GraphZipWithSpec extends TwoStreamsSetup {
+
+  override type Outputs = Int
+
+  override def fixture(b: FlowGraphBuilder): Fixture = new Fixture(b: FlowGraphBuilder) {
+    val zip = ZipWith((_: Int) + (_: Int))(b)
+    override def left: InPort[Int] = zip.in1
+    override def right: InPort[Int] = zip.in2
+    override def out: OutPort[Int] = zip.out
+  }
+
+  "ZipWith" must {
+
+    "work in the happy case" in {
+      val probe = StreamTestKit.SubscriberProbe[Outputs]()
+
+      FlowGraph { implicit b ⇒
+        val zip = ZipWith((_: Int) + (_: Int))
+        Source(1 to 4) ~> zip.in1
+        Source(10 to 40 by 10) ~> zip.in2
+
+        zip.out ~> Sink(probe)
+      }.run()
+
+      val subscription = probe.expectSubscription()
+
+      subscription.request(2)
+      probe.expectNext(11)
+      probe.expectNext(22)
+
+      subscription.request(1)
+      probe.expectNext(33)
+      subscription.request(1)
+      probe.expectNext(44)
+
+      probe.expectComplete()
+    }
+
+    "work in the sad case" in {
+      val probe = StreamTestKit.SubscriberProbe[Outputs]()
+
+      FlowGraph { implicit b ⇒
+        val zip = ZipWith[Int, Int, Int]((_: Int) / (_: Int))
+
+        Source(1 to 4) ~> zip.in1
+        Source(-2 to 2) ~> zip.in2
+
+        zip.out ~> Sink(probe)
+      }.run()
+
+      val subscription = probe.expectSubscription()
+
+      subscription.request(2)
+      probe.expectNext(1 / -2)
+      probe.expectNext(2 / -1)
+
+      subscription.request(2)
+      probe.expectError() match {
+        case a: java.lang.ArithmeticException ⇒ a.getMessage should be("/ by zero")
+      }
+      probe.expectNoMsg(200.millis)
+    }
+
+    commonTests()
+
+    "work with one immediately completed and one nonempty publisher" in {
+      val subscriber1 = setup(completedPublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectCompletedOrSubscriptionFollowedByComplete()
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), completedPublisher)
+      subscriber2.expectCompletedOrSubscriptionFollowedByComplete()
+    }
+
+    "work with one delayed completed and one nonempty publisher" in {
+      val subscriber1 = setup(soonToCompletePublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectCompletedOrSubscriptionFollowedByComplete()
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), soonToCompletePublisher)
+      subscriber2.expectCompletedOrSubscriptionFollowedByComplete()
+    }
+
+    "work with one immediately failed and one nonempty publisher" in {
+      val subscriber1 = setup(failedPublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectErrorOrSubscriptionFollowedByError(TestException)
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), failedPublisher)
+      subscriber2.expectErrorOrSubscriptionFollowedByError(TestException)
+    }
+
+    "work with one delayed failed and one nonempty publisher" in {
+      val subscriber1 = setup(soonToFailPublisher, nonemptyPublisher(1 to 4))
+      subscriber1.expectErrorOrSubscriptionFollowedByError(TestException)
+
+      val subscriber2 = setup(nonemptyPublisher(1 to 4), soonToFailPublisher)
+      val subscription2 = subscriber2.expectErrorOrSubscriptionFollowedByError(TestException)
+    }
+
+    "zipWith a ETA expanded Person.apply (3 inputs)" in {
+      val probe = StreamTestKit.SubscriberProbe[Person]()
+
+      case class Person(name: String, surname: String, int: Int)
+
+      FlowGraph { implicit b ⇒
+        val zip = ZipWith(Person.apply _)
+
+        Source.single("Caplin") ~> zip.in1
+        Source.single("Capybara") ~> zip.in2
+        Source.single(3) ~> zip.in3
+
+        zip.out ~> Sink(probe)
+      }.run()
+
+      val subscription = probe.expectSubscription()
+
+      subscription.request(5)
+      probe.expectNext(Person("Caplin", "Capybara", 3))
+
+      probe.expectComplete()
+    }
+
+    "work with up to 22 inputs" in {
+      val probe = StreamTestKit.SubscriberProbe[String]()
+
+      FlowGraph { implicit b ⇒
+
+        val sum19 = (v1: Int, v2: String, v3: Int, v4: String, v5: Int, v6: String, v7: Int, v8: String, v9: Int, v10: String,
+          v11: Int, v12: String, v13: Int, v14: String, v15: Int, v16: String, v17: Int, v18: String, v19: Int) ⇒
+          v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10 +
+            v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18 + v19
+
+        // odd input ports will be Int, even input ports will be String
+        val zip = ZipWith(sum19)
+
+        Source.single(1) ~> zip.in1
+        Source.single(2).map(_.toString) ~> zip.in2
+        Source.single(3) ~> zip.in3
+        Source.single(4).map(_.toString) ~> zip.in4
+        Source.single(5) ~> zip.in5
+        Source.single(6).map(_.toString) ~> zip.in6
+        Source.single(7) ~> zip.in7
+        Source.single(8).map(_.toString) ~> zip.in8
+        Source.single(9) ~> zip.in9
+        Source.single(10).map(_.toString) ~> zip.in10
+        Source.single(11) ~> zip.in11
+        Source.single(12).map(_.toString) ~> zip.in12
+        Source.single(13) ~> zip.in13
+        Source.single(14).map(_.toString) ~> zip.in14
+        Source.single(15) ~> zip.in15
+        Source.single(16).map(_.toString) ~> zip.in16
+        Source.single(17) ~> zip.in17
+        Source.single(18).map(_.toString) ~> zip.in18
+        Source.single(19) ~> zip.in19
+
+        zip.out ~> Sink(probe)
+      }.run()
+
+      val subscription = probe.expectSubscription()
+
+      subscription.request(1)
+      probe.expectNext((1 to 19).mkString(""))
+
+      probe.expectComplete()
+
+    }
+
+  }
+
+}
